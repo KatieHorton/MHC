fb7007c89ed15aed7be7e84b6cc8ca3489b16821
Katie Horton <kmazurki@gmail.com>
2023-10-10 14:45:39 -0500

    changes

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const { MongoClient } = require("mongodb");
+const process = require('./process/.env');
 require('./models/provider.model.js');
 
-mongoose.connect('mongodb+srv://devLadyKatie:YBhYi3cqZyXm6Ae@cluster0.87m7nu3.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 
 const db = mongoose.connection;
@@ -13,14 +14,14 @@ db.on('error', console.error.bind(console, 'MHC connection error, Threat Level: 
 
 // db.collection('Provider').insertOne({
 
-//     "name": "Dr. Pepper",
-//     "profession": "Medicine Man",
-//     "specialties": ["soda", "bubbles"],
-//     "email": "pepper@doctorOffice.com",
-//     "phone": "999 - 7777",
-//     "address": "123 Cherry St.",
-//     "password": "123cherry",
-//     "coments": [{ "comment": "This man is a genius!", "author": "itsfakemon" }]
+    // "name": "Dr. Pepper",
+    // "profession": "Medicine Man",    
+    // "specialties": ["soda", "bubbles"],
+    // "email": "pepper@doctorOffice.com",
+    // "phone": "999 - 7777",
+    // "address": "123 Cherry St.",
+    // "password": "123cherry",
+    // "coments": [{ "comment": "This man is a genius!", "author": "itsfakemon" }]
 // });
 
 // const cursor = db.collection('Provider').find({ name: 'Dr. Pepper' });
